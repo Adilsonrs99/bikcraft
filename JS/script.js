f7833a7a82fadac0b3014599bd92fe3d95d81fd6
Adilsonrs99 <adilsonrs99@outlook.com>
2023-05-28 10:59:38 -0300

    ativar as perguntas

--- JS/script.js
+++ JS/script.js
@@ -11,6 +11,8 @@ function ativarLink(link) {
 
 links.forEach(ativarLink);
 
+
+
 // ativar itens orçamento
 
 const parametros = new URLSearchParams(location.search);
@@ -25,3 +27,29 @@ function ativarProdutos(parametro) {
 
 
 parametros.forEach(ativarProdutos)
+
+
+
+// ativa perguntas
+
+const perguntas = document.querySelectorAll('.perguntas button');
+
+function ativarPerguntas(event){
+    const pergunta = event.currentTarget;
+    const controls = pergunta.getAttribute("aria-controls");
+    const resposta = document.getElementById(controls)
+
+    
+    resposta.classList.toggle("ativa")
+    const ativa = resposta.classList.contains("ativa")
+    pergunta.setAttribute("aria-expanded", ativa)
+}
+
+
+function eventoPerguntas(pergunta) {
+    pergunta.addEventListener('click', ativarPerguntas)
+}
+
+
+perguntas.forEach(eventoPerguntas);
+
